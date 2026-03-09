75517a86b8214295d64026ea1721738bde81956e
Vaibhav Garad <vaibhavgarad556@gmail.com>
2026-03-09 23:31:39 +0530

    my resume

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -70,8 +70,8 @@ const Navbar = () => {
 
               {/* Download Resume */}
               <a
-                href="src/assets/vaibhav_resume.pdf"
-                download
+                href="/vaibhav_resume.pdf"
+                download="vaibhav_resume.pdf"
                 className="btn btn-sm d-flex align-items-center justify-content-center btn-outline-info"
                 title="Download Resume"
                 style={{
